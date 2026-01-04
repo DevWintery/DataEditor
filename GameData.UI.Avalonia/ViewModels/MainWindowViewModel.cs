8f1fc0435c8583bce68660fb596d2d341ae2c586
DevWintery <dev.wintery@gmail.com>
2026-01-05 03:13:52 +0900

    Initial commit
    Crate Avalonia Project

---
+++ GameData.UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace GameData.UI.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ GameData.UI.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GameData.UI.Avalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
